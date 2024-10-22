737e550839314d29afa9a5719b5ec871af983c59
Misha Dvali <mykhailo.dvali@gmail.com>
2024-10-22 22:36:14 +0300

    Added SASS support + settings for turbo on sass and example of a page

---
+++ src/app/example/page.tsx
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import './style.scss';
+
+function Page() {
+  return (
+    <div className="container">
+      <h1>Hello, Sass in React!</h1>
+      <button className="button">Click Me</button>
+    </div>
+  );
+}
+
+export default Page;
+
