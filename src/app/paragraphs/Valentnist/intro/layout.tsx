e2a6284747316bed7e6370f5cf0d7c8f3f29993c
Misha Dvali <mykhailo.dvali@gmail.com>
2024-10-30 21:20:28 +0200

    Colors fixed

---
+++ src/app/paragraphs/Valentnist/intro/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/paragraphs/Valentnist/intro/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (<>
+					<h1> Empty for now </h1>
+					</>
+  );
+}
